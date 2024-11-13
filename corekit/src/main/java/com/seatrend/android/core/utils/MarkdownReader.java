c5a21c073ebe21fa00b4c5432198434d83fbfe02
ssmalllucky <ssmalllucky@163.com>
2024-11-13 11:41:30 +0800

    refactor(MarkdownReader): MarkdownReader 类函数解耦，以及主要函数添加注释。

--- corekit/src/main/java/com/seatrend/android/core/utils/MarkdownReader.java
+++ corekit/src/main/java/com/seatrend/android/core/utils/MarkdownReader.java
@@ -10,6 +10,7 @@ import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 /**
@@ -52,72 +53,55 @@ public class MarkdownReader {
     }
 
     /**
-     * 读取 Markdown 格式文件的指定部分
+     * 从输入流中读取 Markdown 格式的更新日志信息，并解析到 UpdateEntity 对象中
+     * 该方法主要用于解析 CHANGELOG.md 文件中的更新信息，以便在应用中展示
      *
-     * @param context  上下文对象，用于访问应用程序资源
-     * @param fileName 要读取的文件名
-     * @return 返回一个 UpdateEntity 对象，其中包含读取的 Markdown 内容和版本信息
+     * @param inputStream 输入流，通常来源于 CHANGELOG.md 文件
+     * @return 解析后的 UpdateEntity 对象，包含版本号和更新内容
      */
-    public static UpdateEntity readMarkdownSections(Context context, String fileName) {
+    public static UpdateEntity readMarkdownSections(InputStream inputStream) {
         UpdateEntity updateEntity = new UpdateEntity();
         InputStreamReader inputReader;
         SpannableStringBuilder builder = new SpannableStringBuilder();
         try {
-            inputReader = new InputStreamReader(context.getResources().getAssets().open(fileName));
+            inputReader = new InputStreamReader(inputStream);
             BufferedReader reader = new BufferedReader(inputReader);
             String line;
             boolean hasReadRoot = false;
-            boolean isSection = false;
 
-            SpannableString spannableString;
+            int lineNumber = 0;
+
+            Log.d(TAG, "Starting to read the file contents through the while loop...");
 
             while ((line = reader.readLine()) != null) {
-                Log.d(TAG, "readMarkdownSections -> line: " + line);
+                Log.d(TAG, "lineNumber: " + (lineNumber++));
+                // 检查是否是版本号
                 if (line.startsWith("### ")) {
+                    // 确保只读取一次根版本号
                     if (hasReadRoot) {
                         break;
                     } else {
                         hasReadRoot = true;
                     }
-                    if (isSection) {
-                        builder.append("\n");
-                    }
+
+                    // 提取版本号
                     String versionMD = line.replace("###", "").trim();
                     updateEntity.setVersionName(versionMD.startsWith("V") ? versionMD : "V" + versionMD);
-                    isSection = true;
                 } else if (startsWithDigit(line)) {
-                    String replaced = line.replaceAll("- ", "•");
-                    replaced = replaced.replaceAll("\\**", "");
-                    spannableString = new SpannableString(replaced);
-                    spannableString.setSpan(new StyleSpan(Typeface.NORMAL), 0, replaced.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                    builder.append(spannableString);
-                    builder.append("\n");
+                    resolveStartsWithDigit(line, builder);
                 } else if (line.startsWith("- ")) {
-                    String replaced = line.replaceAll("- ", "•");
-                    replaced = replaced.replaceAll("\\**", "");
-                    spannableString = new SpannableString(replaced);
-                    spannableString.setSpan(new StyleSpan(Typeface.NORMAL), 0, replaced.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                    builder.append(spannableString);
-                    builder.append("\n");
+                    resolveStartWithDash(line, builder);
                 } else if (line.contains(":")) {
-                    String replaced = line.substring(line.indexOf(":") + 1).trim();
-                    replaced = replaced.replaceAll("\\**", "");
-                    spannableString = new SpannableString(replaced);
-                    spannableString.setSpan(new StyleSpan(Typeface.NORMAL), 0, replaced.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                    builder.append(spannableString);
-                    builder.append("\n");
+                    resolveStartsWithColon(line, builder);
                 } else if (line.startsWith("【")) {
-                    String replaced = line.substring(line.indexOf("【") + 1).trim();
-                    replaced = replaced.replaceAll("】", "");
-                    spannableString = new SpannableString(replaced);
-                    spannableString.setSpan(new StyleSpan(Typeface.BOLD), 0, replaced.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                    builder.append(spannableString);
-                    builder.append("\n");
+                    resolveStartsSquareBracketsLeft(line, builder);
+                }
+                // 以“--”开头的行不会被 Markdown 进行标记和格式化，所以适合作为不想返回在结果中的行（但可以出现在git提交内容
+                // 和 CHANGELOG.md文件中
+                else if (line.startsWith("--")) {
+                    builder.append("");
                 } else {
-                    spannableString = new SpannableString(line);
-                    spannableString.setSpan(new StyleSpan(Typeface.NORMAL), 0, line.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                    builder.append(spannableString);
-                    builder.append("\n");
+                    resolveNormalLine(line, builder);
                 }
             }
             reader.close();
@@ -131,17 +115,108 @@ public class MarkdownReader {
     }
 
     /**
-     * 检查字符串是否以数字开始
-     * <p>
-     * 此方法通过正则表达式验证字符串的开头是否为数字（0-9）它首先检查输入字符串是否为null，
-     * 然后使用matches方法和正则表达式"^[0-9].*"来确定字符串是否以数字开头正则表达式的含义是：
-     * ^ 表示字符串的开始，
-     * [0-9] 表示任何一个数字字符，
-     * .* 表示任意数量的任何字符
+     * 格式化以数字字符开头的行，并移除任何星号，处理完成后添加换行符，最终返回格式化后的文本
      *
-     * @param input 要检查的字符串
-     * @return 如果字符串不为null且以数字开始，则返回true；否则返回false
+     * @param line    需要处理的原始字符串，检查并替换特定开头字符
+     * @param builder 用于累积处理后文本的SpannableStringBuilder对象
      */
+    private static void resolveStartsWithDigit(String line, SpannableStringBuilder builder) {
+        // 将行首的破折号和空格替换为项目符号符号
+        String replaced = line.replaceAll("- ", "•");
+        // 移除所有的星号
+        replaced = replaced.replaceAll("\\**", "");
+        // 创建一个新的SpannableString对象来保存替换后的字符串
+        SpannableString spannableString = new SpannableString(replaced);
+        // 设置整个字符串的样式为正常，以统一格式
+        spannableString.setSpan(new StyleSpan(Typeface.NORMAL), 0, replaced.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        // 将格式化后的文本追加到构建器中
+        builder.append(spannableString);
+        // 追加换行符以分隔不同的文本行
+        builder.append("\n");
+    }
+
+    /**
+     * 处理以“- ”开头的行，并移除多余的星号，处理完成后添加换行符，最终返回格式化后的SpannableStringBuilder
+     *
+     * @param line    需要处理的字符串
+     * @param builder SpannableStringBuilder实例，用于收集和格式化处理后的文本
+     */
+    private static void resolveStartWithDash(String line, SpannableStringBuilder builder) {
+        // 将破折号和空格替换为子弹点
+        String replaced = line.replaceAll("- ", "•");
+        // 移除所有多余的星号
+        replaced = replaced.replaceAll("\\**", "");
+        // 创建一个SpannableString实例用于进一步的格式化
+        SpannableString spannableString = new SpannableString(replaced);
+        // 设置文本为普通样式
+        spannableString.setSpan(new StyleSpan(Typeface.NORMAL), 0, replaced.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        // 将格式化后的文本追加到builder中
+        builder.append(spannableString);
+        // 添加换行符以区分不同的文本行
+        builder.append("\n");
+    }
+
+    /**
+     * 处理以“:”开头的行，并移除多余的星号，处理完成后添加换行符，最终返回格式化后的SpannableStringBuilder
+     *
+     * @param line    需要处理的文本行，预期以冒号开头
+     * @param builder SpannableStringBuilder实例，用于追加格式化后的文本
+     */
+    private static void resolveStartsWithColon(String line, SpannableStringBuilder builder) {
+        // 移除冒号和其后可能存在的多余星号，并修剪前导空格
+        String replaced = line.substring(line.indexOf(":") + 1).trim();
+        replaced = replaced.replaceAll("\\**", "");
+
+        // 创建SpannableString实例，用于设置文本样式
+        SpannableString spannableString = new SpannableString(replaced);
+
+        // 设置文本为正常样式
+        spannableString.setSpan(new StyleSpan(Typeface.NORMAL), 0, replaced.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        // 将格式化后的文本追加到builder中，并添加换行符
+        builder.append(spannableString);
+        builder.append("\n");
+    }
+
+    /**
+     * 处理以“【”开头的行，并移除多余的星号，处理完成后添加换行符，最终返回格式化后的SpannableStringBuilder
+     *
+     * @param line    包含“【” 字符的原始字符串
+     * @param builder SpannableStringBuilder对象，用于接收格式化后的文本
+     */
+    private static void resolveStartsSquareBracketsLeft(String line, SpannableStringBuilder builder) {
+        // 移除【】符号并提取其中的内容
+        String replaced = line.substring(line.indexOf("【") + 1).trim();
+        replaced = replaced.replaceAll("】", "");
+
+        // 创建SpannableString对象以对提取的文本进行格式化
+        SpannableString spannableString = new SpannableString(replaced);
+
+        // 设置文本为粗体格式
+        spannableString.setSpan(new StyleSpan(Typeface.BOLD), 0, replaced.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        // 将格式化后的文本追加到builder中，并添加换行符以分隔不同的文本块
+        builder.append(spannableString);
+        builder.append("\n");
+    }
+
+    /**
+     * 处理正常文本行，为其添加普通样式并追加换行符
+     *
+     * @param line    需要处理的文本行
+     * @param builder 用于追加文本的SpannableStringBuilder对象
+     */
+    private static void resolveNormalLine(String line, SpannableStringBuilder builder) {
+        // 创建一个SpannableString对象，用于设置文本样式
+        SpannableString spannableString = new SpannableString(line);
+        // 为整个文本设置普通样式
+        spannableString.setSpan(new StyleSpan(Typeface.NORMAL), 0, line.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        // 将格式化后的文本追加到builder中
+        builder.append(spannableString);
+        // 追加换行符，以便于下一行文本的显示
+        builder.append("\n");
+    }
+
     public static boolean startsWithDigit(String input) {
         return input != null && input.matches("^[0-9].*");
     }
